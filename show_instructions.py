5cb13d0ca5ac4390b6e2eb0ac7fdbf333eeb683f
Cylus123 <134886377+Cylus123@users.noreply.github.com>
2023-06-03 07:52:24 +0000

    yes_no function removed print statement to be more flexible if needed later in the program.

--- show_instructions.py
+++ show_instructions.py
@@ -5,15 +5,17 @@ def yes_no(question):
     watched_before = input(question).lower()
   
     if watched_before == "yes" or watched_before == "y":
-      print("Continue with program")
       return watched_before
+      
     elif watched_before == "no" or watched_before == "n":
-      print("Display instructions")
       return watched_before
+    
     else:
       print("Invalid input please enter yes or no")
 
 valid = False 
 while not valid:
   show_instructions = yes_no("Have you watched this before ?: ")
+  if show instructions == "no":
+    print("Instructions")
       
