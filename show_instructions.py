b0d5cdf37b504b99ef6f695dcfdbe2b52ec88bc7
Cylus123 <134886377+Cylus123@users.noreply.github.com>
2023-06-03 07:39:28 +0000

    yes no function

--- show_instructions.py
+++ show_instructions.py
@@ -1,14 +1,19 @@
+def yes_no(question):
+
+  valid = False 
+  while not valid:
+    watched_before = input(question).lower()
+  
+    if watched_before == "yes" or watched_before == "y":
+      print("Continue with program")
+      return watched_before
+    elif watched_before == "no" or watched_before == "n":
+      print("Display instructions")
+      return watched_before
+    else:
+      print("Invalid input please enter yes or no")
+
 valid = False 
 while not valid:
-  watched_before = input("Have you seen a movie here before ").lower()
-
-  if watched_before == "yes":
-    print("Continue with program")
-  elif watched_before == "y":
-    print("Continue with program")
-  elif watched_before == "no":
-    print("Display instructions")
-  elif watched_before == "n":
-    print("Display instructions")
-  else:
-    print("Invalid input please enter yes or no")
+  show_instructions = yes_no("Have you watched this before ?: ")
+      
